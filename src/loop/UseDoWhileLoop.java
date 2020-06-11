8206eb83672e8e1815ac44e23b17dbfddc53fe98
mrahman <rahmanww@gmail.com>
2020-06-10 22:48:11 -0400

    added one line in do...while

--- src/loop/UseDoWhileLoop.java
+++ src/loop/UseDoWhileLoop.java
@@ -5,6 +5,7 @@ public class UseDoWhileLoop {
 	public static void main(String[] args) {
 		int num = 10;
 		do {
+			System.out.println("my first task");
 			System.out.println(num);
 			num--;
 		}while(num>0);
